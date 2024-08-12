01af45bb8e678e428d977cf1c074a92e35a845b4
Valentinux <107010509+valentinux@users.noreply.github.com>
2024-08-12 15:12:12 +0200

    Initial Commit

---
+++ ScrollViews-lessons/ScrollViews_lessonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollViews_lessonsApp.swift
+//  ScrollViews-lessons
+//
+//  Created by Valentin Fernandez on 12/8/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrollViews_lessonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
