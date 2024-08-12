b42cfbdeab525a0859f2ac7943030cea143ed983
Valentinux <107010509+valentinux@users.noreply.github.com>
2024-08-12 16:13:40 +0200

    Secon commit, desing the card view

--- ScrollViews-lessons/CardView.swift
+++ ScrollViews-lessons/CardView.swift
@@ -9,9 +9,45 @@ import SwiftUI
 
 struct CardView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack {
+            Image("swift5")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            
+            HStack {
+                VStack(alignment: .leading){
+                    
+                    Text("Curso de swift 5")
+                        .font(.largeTitle)
+                        .foregroundColor(.secondary)
+                    
+                    Text("Aprende el lenguaje de programación de Apple desde 0")
+                        .font(.title2)
+                        .foregroundColor(.primary)
+                        .lineLimit(3)
+                    
+                    Text("Nivel: Fácil")
+                        .font(.headline)
+                        .foregroundColor(.secondary)
+                }
+                
+                Spacer()
+            }
+            .padding()
+        }
+        .cornerRadius(16)
+        .overlay(
+            RoundedRectangle(cornerRadius: 16)
+                .stroke(Color(.sRGB, red:120/255, green: 40/255, blue:140/255, opacity: 0.6) ,lineWidth: 4))
+        .padding([.top, .horizontal])
+        
     }
 }
+    
+
+    
+    
 
 #Preview {
     CardView()
