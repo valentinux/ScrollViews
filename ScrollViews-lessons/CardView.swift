77eac5cffb4c5885f0b6bd0b40e315e1de095a68
Valentinux <107010509+valentinux@users.noreply.github.com>
2024-08-12 16:08:43 +0200

    Secon commit, desing the card view

---
+++ ScrollViews-lessons/CardView.swift
@@ -0,0 +1,18 @@
+//
+//  CardView.swift
+//  ScrollViews-lessons
+//
+//  Created by Valentin Fernandez on 12/8/24.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardView()
+}
