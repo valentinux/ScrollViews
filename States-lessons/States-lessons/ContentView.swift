85fea32172be01437e2061ba89ea7ec072b08d68
Valentinux <107010509+valentinux@users.noreply.github.com>
2024-08-21 15:55:36 +0200

    I implement the toggle button play/stop and the counter button

--- States-lessons/States-lessons/ContentView.swift
+++ States-lessons/States-lessons/ContentView.swift
@@ -8,14 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isPlaying: Bool = true
+    @State private var counter: Int = 0
+    
     var body: some View {
         VStack {
             Button{
-                // TODO: Cambiar el play por stop según el estado
-                print("Botón presionado...")
+                
+                isPlaying.toggle()
             }
         label: {
-            Image(systemName: "play.circle")
+            Image(systemName: isPlaying ? "stop.circle" : "play.circle")
             
         }
         .padding()
@@ -23,7 +27,24 @@ struct ContentView: View {
         .font(.system(size: 80))
         .background(Color.black)
         .clipShape(Ellipse())
-        .foregroundStyle(Color.red)
+        .foregroundStyle(isPlaying ? Color.white : Color.blue)
+        .padding(.bottom)
+        
+        Button{
+            counter += 1
+        }
+    label: {
+        Circle()
+            .frame(width: 130, height: 150)
+            .overlay(
+                Text("\(counter)")
+                    .font(.system(size: 60, weight: .bold, design: .rounded))
+                    .foregroundStyle(Color.black)
+                )
+            
+        
+    }
+    
     }
 }
 
